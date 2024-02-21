0c4408473ad2efa344a8532a477cf2fedbd5272b
ghazalasfr <ghazal.lsfr@gmail.com>
2024-02-21 12:30:31 -0800

    done

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,17 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""init model
 
 """
-unique `FileStorage` instance for the HBNB application
-
-"""
-
+import os
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
-storage = FileStorage()
+
+storage_type = os.environ.get("HBNB_TYPE_STORAGE")
+
+if storage_type == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
