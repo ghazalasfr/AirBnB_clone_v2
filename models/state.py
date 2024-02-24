308498b9933d8217da083ad02d720433ed5984ea
ghazalasfr <137912705+ghazalasfr@users.noreply.github.com>
2024-02-24 23:40:53 +0100

    Add files via upload

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for the State class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    the state class with :
+    Attributes :name
+    """
+
+    name = ""
